316818f3f18644c147253571bd6ef6218ac00a19
Andrey Skryl <ya.skryl@yandex.ru>
2017-06-17 21:44:14 +0700

    Configure base project and create Hello World controller.

---
+++ src/main/java/com/qoobico/remind/server/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.qoobico.remind.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.qoobico.remind.server")
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
